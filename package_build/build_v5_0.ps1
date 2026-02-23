a4df78cf55a286b7e4d12e18c316ea2044e17bb8
greovity <16100498+greovity@user.noreply.gitee.com>
2026-02-23 19:05:41 +0800

    update

--- package_build/build_v5_0.ps1
+++
@@ -1,7 +0,0 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = "Stop"
-
-$RepoRoot = "D:\lanzou_manga_downloader"
-$SpecPath = Join-Path $RepoRoot "package_build\lanzou_downloader_gui_v5_0.spec"
-
-pyinstaller --clean --noconfirm $SpecPath
